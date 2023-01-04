07a8af6a87b680bb4e9cdc8a54231171cc496cae
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-04 22:35:27 +0000

    Add widgets to forms for styling

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,5 +1,3 @@
-from dataclasses import fields
-from pyexpat import model
 from django import forms
 from . models import Ingredient, MenuItem, RecipeRequirement, Purchases
 
@@ -7,6 +5,13 @@ class IngredientCreate(forms.ModelForm):
     class Meta:
         model = Ingredient
         fields = '__all__'
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            'unit': forms.TextInput(attrs={'class': 'form-control'}),
+            'unit_price': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
 class IngredientUpdate(forms.ModelForm):
     class Meta:
@@ -18,12 +23,27 @@ class MenuCreate(forms.ModelForm):
         model = MenuItem
         fields = '__all__'
 
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'price':forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
 class PurchaseCreate(forms.ModelForm):
     class Meta:
         model = Purchases
         fields = ["menu_item"]
 
+        widgets = {
+            'menu_item':forms.Select(attrs={'class': 'form-control'}),
+        }
+
 class RecipeRequirementCreate(forms.ModelForm):
     class Meta:
         model = RecipeRequirement
         fields = '__all__'
+
+        widgets = {
+            'menu_item':forms.Select(attrs={'class': 'form-control'}),
+            'ingredient': forms.Select(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
