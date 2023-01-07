63249f38e426a09aa9946b55b2d33236e0241758
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-07 02:47:06 +0000

    Add url path for ingredient deletion

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path("ingredients/", views.IngredientList.as_view(), name="ingredientlist"),
     path("ingredients/create", views.IngredientCreateView.as_view(), name="ingredientcreate"),
     path("ingredients/update/<pk>", views.IngredientUpdateView.as_view(), name="ingredientupdate"),
+    path("ingredients/delete/<pk>", views.IngredientDeleteView.as_view(), name="ingredientdelete"),
     path("menu/create", views.MenuCreateView.as_view(),name="menucreate"),
     path("menu/", views.MenuList.as_view(), name="menulist"),
     path("purchases/create", views.PurchasesCreateView.as_view(), name="purchasecreate"),
     path("purchases/", views.PurchasesList.as_view(), name="purchaselist"),
-    path("recipe/create", views.RecipeRequirementView.as_view(), name="recipecreate")
+    path("recipe/create", views.RecipeRequirementView.as_view(), name="recipecreate"),
 ]
