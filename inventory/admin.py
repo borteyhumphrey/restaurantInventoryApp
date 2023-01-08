19d3a681406937157022c3c1e67affe48267231b
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-08 19:32:07 +0000

    Register database models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Ingredient, MenuItem, RecipeRequirement, Purchases
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchases)
