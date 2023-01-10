d9cbc5d2a58dd893e8c3a9abc3ca3e4da01c8fea
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-10 23:20:33 +0000

    Add log out view

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
@@ -10,6 +10,10 @@ from . forms import IngredientCreate, IngredientUpdate, MenuCreate, PurchaseCrea
 @login_required
 def home(request):
     return render(request, "inventory/home.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
 
 # Create your views here.
 class IngredientList(LoginRequiredMixin,ListView):
